1bb9033f810dbc2b1f91fefcbfb3a36efd363fca
AlexisKiehn <62058880+AlexisKiehn@users.noreply.github.com>
2025-11-28 13:02:16 +0100

    Clean up app.py by removing unnecessary comments
    Removed introductory markdown and comments about the app's functionality.

--- app.py
+++ app.py
@@ -7,24 +7,6 @@ import pandas as pd
 # TaxiFareModel front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
 with st.form(key='params_form'):
     col1, col2 = st.columns(2)
 
@@ -41,18 +23,10 @@ with st.form(key='params_form'):
 
     submit_button = st.form_submit_button(label='Predict Fare')
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
 url = 'https://taxifare.lewagon.ai/predict'
 
 if url == 'https://taxifare.lewagon.ai/predict':
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
 if submit_button:
     # 2. Let's build a dictionary containing the parameters for our API...
@@ -72,9 +46,7 @@ if submit_button:
     if response.status_code == 200:
         prediction = response.json().get('fare', 0)
 
-        '''
-        ## Finally, we can display the prediction to the user
-        '''
+        
         st.metric(label="Predicted Fare", value=f"${round(prediction, 2)}")
 
         # Bonus: Map
